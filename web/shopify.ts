85a788d64fa5668992daa32d3e2265d0061884eb
payoottd9527 <payoottd@gmail.com>
2025-07-17 21:43:53 +0800

    Initialize spike_using_typescript

---
+++ web/shopify.ts
@@ -0,0 +1,36 @@
+import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
+import { ShopifyApp, shopifyApp } from "@shopify/shopify-app-express";
+import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
+let { restResources } = await import(
+  `@shopify/shopify-api/rest/admin/${LATEST_API_VERSION}`
+);
+// If you want IntelliSense for the rest resources, you should import them directly
+// import { restResources } from "@shopify/shopify-api/rest/admin/2022-10";
+
+const DB_PATH = `${process.cwd()}/database.sqlite`;
+
+const shopify = shopifyApp({
+  api: {
+    apiVersion: LATEST_API_VERSION,
+    restResources,
+    // Uncomment to enable calling the billing functions
+    // billing: {
+    //   "My Shopify One-Time Charge": {
+    //     amount: 5.0,
+    //     currencyCode: "USD",
+    //     interval: BillingInterval.OneTime,
+    //   },
+    // },
+  },
+  auth: {
+    path: "/api/auth",
+    callbackPath: "/api/auth/callback",
+  },
+  webhooks: {
+    path: "/api/webhooks",
+  },
+  // This should be replaced with your preferred storage strategy
+  sessionStorage: new SQLiteSessionStorage(DB_PATH),
+});
+
+export default shopify;
