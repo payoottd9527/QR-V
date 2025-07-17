915ae2062719f722203af2be2d2039246bbe380d
payoottd9527 <payoottd@gmail.com>
2025-07-17 21:43:48 +0800

    Initialize kos/update_to_use_session_storage_packages

---
+++ web/shopify.js
@@ -0,0 +1,16 @@
+import { LogSeverity } from "@shopify/shopify-api";
+import { restResources } from "@shopify/shopify-api/rest/admin/2022-10";
+import { shopifyApp } from "@shopify/shopify-app-express";
+import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
+
+const DB_PATH = `${process.cwd()}/database.sqlite`;
+
+const shopify = shopifyApp({
+  api: {
+    restResources,
+  },
+  // This should be replaced with your preferred storage strategy
+  sessionStorage: new SQLiteSessionStorage(DB_PATH),
+});
+
+export default shopify;
